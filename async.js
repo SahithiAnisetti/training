b4d68314cbf0e5100c1b3d7331c2f8bcbe830261
SahithiAnisetti <anisettisahithi940@gmail.com>
2025-01-22 10:26:09 +0530

    javascript

--- async.js
+++ async.js
@@ -13,25 +13,53 @@
 // let p1 = new Promise((resolved ,reject)=>{});
 // console.log(p1);
 
-let p2 = new Promise((resolve , reject)=>{
-    resolve("Success");
-});
-console.log(p2);
-p2.then((response)=>
-    {
-        console.log(response);
-}).catch((error)=>{
-    console.log(error);
-}).finally(()=>console.log("finally printing for both"))
+// let p2 = new Promise((resolve , reject)=>{
+//     resolve("Success");
+// });
+// console.log(p2);
+// p2.then((response)=>
+//     {
+//         console.log(response);
+// }).catch((error)=>{
+//     console.log(error);
+// }).finally(()=>console.log("finally printing for both"))
 
 
 
-let p3 = new Promise((resolve, reject)=>{
-    reject("Failures");
-});
-// console.log(p3);
-p3.then((data)=>{console.log(data);})
-.catch((error)=>{
-    console.log(error);
-}).finally(()=>console.log("finally printing for both"))
-
+// let p3 = new Promise((resolve, reject)=>{
+//     reject("Failures");
+// });
+// // console.log(p3);
+// p3.then((data)=>{console.log(data);})
+// .catch((error)=>{
+//     console.log(error);
+// }).finally(()=>console.log("finally printing for both"))
+//API fetching
+//console.log(window);
+function fetchUsers(){
+    // let res = window.fetch("https://api.github.com/users");
+    let res = window.fetch("https://jsonplaceholder.typicode.com/users");
+    // console.log(res);
+    res.then((response)=>{
+        // console.log(response);
+        // console.log(response.json());
+        response.json().then(data=>{
+            console.log(data);
+            let store=document.getElementById("store");
+            // console.log(store);
+            data.map((user)=>{
+                // console.log(user);
+                store.innerHTML += `
+                <tr>
+                <td>${user.id}</td>
+                <td>${user.name}</td>
+                <td>${user.email}</td>
+                <td>${user.company.name}</td>
+                </tr>
+                ` 
+            })
+        })
+    })
+    .catch(err=>console.log(err))
+}
+fetchUsers()
