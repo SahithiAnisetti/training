6917dc08076e82c5acd0734e544a8912a3469413
SahithiAnisetti <anisettisahithi940@gmail.com>
2025-01-24 20:07:10 +0530

    javascript

--- dom.js
+++ dom.js
@@ -1,9 +1,42 @@
 //Document.getElementId("");
-let element = document.getElementById("demo");
-element.innerText = "DOM";
-console.log(element);
+// let element = document.getElementById("demo");
+// element.innerText = "DOM";
+// console.log(element);
 
-let div = document.getElementById("test");
-//div.innerText = "<h1>Header<h1>"
-div.innerHTML = "<h1>Header<h1>"
-console.log(div);
+/// let element = document.getElementById("demo");
+// element.innnerText="DOM";
+// console.log(element);
+
+// let div=document.getElementById("test");
+// div.innerHTML="<h1>Header</h1>";
+// console.log(div);
+
+// let ele=document.getElementsByClassName("test");
+// // console.log(ele);
+// // console.log(Array.isArray(ele));
+// ele[0].style.backgroundColor="yellow";
+
+// //spread operator
+// let x=[...ele];
+// console.log(x, Array.isArray(x));
+// x.map((element)=>{
+//     // console.log(element);
+//     element.style.backgroundColor="teal";
+// })
+
+// let ele=document.getElementsByTagName("div");
+// console.log(ele);
+
+// let ele=document.querySelector("#demo");
+// console.log(ele);
+
+// let ele=document.querySelectorAll(".test");
+// // console.log(ele);
+// [...ele].map((element)=>{
+//     console.log(element.innerText);
+// })
+
+// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+//////EVENT/////
+let btn = document.getElementById("btn");
+//btn.addEventListener("events", callback)
